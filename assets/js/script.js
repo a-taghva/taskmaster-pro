9ca104a9bc438cbe5eb18b188bb032a4714d7969
Ali Taghva <taghvali99@gmail.com>
2021-01-06 12:50:41 -0500

    edit tasks date and text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-var tasks = {};
+let tasks = {};
 
-var createTask = function(taskText, taskDate, taskList) {
+let createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
-  var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
+  let taskLi = $("<li>").addClass("list-group-item");
+  let taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
-  var taskP = $("<p>")
+  let taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
 
@@ -18,7 +18,7 @@ var createTask = function(taskText, taskDate, taskList) {
   $("#list-" + taskList).append(taskLi);
 };
 
-var loadTasks = function() {
+let loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
   // if nothing in localStorage, create a new object to track all task status arrays
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -41,30 +40,103 @@ var loadTasks = function() {
   });
 };
 
-var saveTasks = function() {
+let saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  let text = $(this)
+    .text()
+    .trim();
 
+  let textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function() {
+  let text = $(this)
+    .val()
+    .trim();
+  
+  let status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  let index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  let taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  $(this).replaceWith(taskP);
+});
+
+
+$(".list-group").on("click", "span", function() {
+  let date = $(this)
+    .text()
+    .trim();
+  
+  let dataInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  $(this).replaceWith(dataInput);
+  dataInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "input", function() {
+  let taskStatus = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  let date = $(this)
+    .val();
+
+  let index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  tasks[taskStatus][index].date = date;
+  saveTasks();
+
+  let dateSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  $(this).replaceWith(dateSpan);
+});
 
 
 // modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
+$("#task-form-modal").on("show.bs.modal",  function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
 });
 
 // modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
+$("#task-form-modal").on("shown.bs.modal",  function() {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-primary").click( function() {
   // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
+  let taskText = $("#modalTaskDescription").val();
+  let taskDate = $("#modalDueDate").val();
 
   if (taskText && taskDate) {
     createTask(taskText, taskDate, "toDo");
@@ -83,8 +155,8 @@ $("#task-form-modal .btn-primary").click(function() {
 });
 
 // remove all tasks
-$("#remove-tasks").on("click", function() {
-  for (var key in tasks) {
+$("#remove-tasks").on("click",  function() {
+  for (let key in tasks) {
     tasks[key].length = 0;
     $("#list-" + key).empty();
   }
